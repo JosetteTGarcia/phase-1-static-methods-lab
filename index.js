897c24bf8fa9207eb264ee08a82099f872422bfb
Josette Garcia <josettetgarcia@gmail.com>
2022-03-01 22:09:08 -0600

    commit tree ec9113974f69a7eb015c31c3767186e6b722f94c

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    // const letters = str.split('');
+    // letters[0].upperCase();
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace(/[^a-z0-9- '']/gi, '')
+    
+  }
+
+  static titleize(string) {
+    let dontCap =  ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let finalString = [];
+    let wordsFromString = string.split(" ");
+    for (let n = 0; n < wordsFromString.length; n++){
+      if (n == 0) {
+        finalString.push(this.capitalize(wordsFromString [n]))
+      } else{
+        if (dontCap.includes(wordsFromString [n])){
+          finalString.push( wordsFromString[ n ] )
+        } else {
+          finalString.push( this.capitalize( wordsFromString[ n ] ) )
+        }
+      }
+    }
+    return finalString.join(" ");
+  }
 }
